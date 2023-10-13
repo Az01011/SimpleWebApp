66a1dcdf279fcab1b053ccb620b6f21908086f36
Abdullah Zaidi <abdullahx575@gmail.com>
2023-10-13 03:19:49 +0100

    Functionality exact same just made it a bit more clear

--- app.py
+++ app.py
@@ -9,27 +9,27 @@ connect_to_database.execute('''
 CREATE TABLE IF NOT EXISTS countries (
     CountryName TEXT UNIQUE NOT NULL
 )
-''')
+''') #single column
 
 # Inserting countries into the table
 country_lst = ['Albania', 'Andorra', 'Australia', 'Brazil', 'Belgium', 'Canada', 'China', 'France', 'Germany', 'India', 'Indonesia', 'Ireland', 'Italy', 'Japan', 'Kenya', 'Luxembourg', 'Mexico', 'New Zealand', 'Nigeria', 'Portugal', 'Russia', 'South Africa', 'South Korea', 'Spain', 'Sweden', 'Thailand', 'Ukraine', 'United Kingdom', 'United States of America', 'Vietnam', 'Zambia']
 for country in country_lst:
     connect_to_database.execute("INSERT OR IGNORE INTO countries (CountryName) VALUES (?)", (country,))
-connect_to_database.commit()
+connect_to_database.commit() #looping and inserting
 
-@app.route('/', methods=['GET', 'POST'])
+#@app - index runs when / is accessed
+@app.route('/', methods=['GET', 'POST']) #single route handls both get and post
 def index():
-    countries = ''
-    if request.method == 'POST':
+    empty_lst = []
+    if request.method == 'POST':  #user input captured
         user_inp = request.form['searchTerm'].lower()
         matching_countries = connect_to_database.execute("SELECT CountryName FROM countries WHERE CountryName LIKE ?", ('%' + user_inp + '%',)).fetchall()
+    
+        for i in matching_countries: 
+            empty_lst.append(i[0])
 
-        #ouputting searched countries to a list
-        matching_countries_list = []
-        for record in matching_countries:
-            matching_countries_list.append(record[0])
-        countries = ', '.join(matching_countries_list)
-    return render_template('index.html', countries=countries)
+    return render_template('index.html', empty_lst)
+
 
 # Running the Flask app
-app.run(debug=True, threaded=False)
+app.run(debug=True, threaded=False) #threaded = false was a fix for error
